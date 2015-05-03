56d33ca5f5fcf0c2adc28bf9fe92a78bfe2e8f71
nimahajan <me@hapuslabs.com>
2015-05-03 12:06:37 -0700

    first commit

---
+++ ShreeramContainer/src/com/shreeramcontainer/main/ShreeramContainerResource.java
@@ -0,0 +1,18 @@
+package com.shreeramcontainer.main;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+ 
+@Path("/hello")
+public class ShreeramContainerResource {
+ 
+	@GET
+	@Path("/{param}")
+	public Response getMsg(@PathParam("param") String msg) {
+		String output = "Hello: " + msg;
+		return Response.status(200).entity(output).build();
+	}
+ 
+}
